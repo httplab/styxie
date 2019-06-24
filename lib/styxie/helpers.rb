f68cfc5681d52a38fd9222ef26427e892f684485
nastyakp <nst.kpv@gmail.com>
2019-06-24 15:39:56 +0300

    Add helper to include styxie to html directly. May be useful when there is no sprockets in app.

--- lib/styxie/helpers.rb
+++ lib/styxie/helpers.rb
@@ -12,5 +12,41 @@ module Styxie
       # TODO: support nested namespaces?
       namespace == controller_path.split('/').first
     end
+
+    def styxie_include
+      result = <<~CODE
+        <script type="text/javascript">
+        if (window.Styxie == null) { window.Styxie = {}; }
+
+        window.Styxie.Initializers = {};
+        if (window.Styxie.initQueue == null) { window.Styxie.initQueue = []; }
+
+        window.Styxie.applyInitializer = function(klass, method, json) {
+          const initializer = this.Initializers[klass];
+          if (!initializer) { return; }
+
+          if (initializer.initialize) {
+            initializer.initialize(json);
+          }
+
+          if (initializer[method]) {
+            return initializer[method](json);
+          }
+        };
+
+        window.Styxie.deferredInit = function() {
+          for (let args of Array.from(this.initQueue)) { this.applyInitializer.apply(this, args); }
+          return this.initQueue = [];
+        };
+
+        window.initStyxie = () => {
+          return this.Styxie.deferredInit();
+        };
+        </script>
+      CODE
+
+      result.html_safe
+    end
+    
   end
 end
